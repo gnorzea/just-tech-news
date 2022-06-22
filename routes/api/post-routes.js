4323bf228a13a1a7d10b9bbdcfa5dbafd6a3e7bd
Thunnder <gnorzea@gmail.com>
2022-06-22 00:41:30 -0400

    comment model creation

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 
 
 // get all users
@@ -22,6 +22,14 @@ router.get('/', (req, res) => {
       ['created_at', 'DESC']
     ],
     include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
       {
         model: User,
         attributes: ['username']
@@ -52,6 +60,14 @@ router.get('/:id', (req, res) => {
       ]
     ],
     include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
       {
         model: User,
         attributes: ['username']
